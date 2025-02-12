feff3656b48f032b18953fd5570ccca566fa7fe4
Charles Oroko <charles.oroko@outlook.com>
2025-02-11 21:36:40 -0800

    wip: begin refactor to svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,80 @@
 <script lang="ts">
-	import { Toaster } from 'svelte-french-toast';
-	import 'cirrus-ui';
-	import { page } from '$app/stores';
+	import { SvelteToast } from '@zerodevx/svelte-toast';
+	import '../app.css';
+	let { children } = $props();
 
-	export let data;
+	let showMenu = $state(false);
+	const toggleShowMenu = () => {
+		showMenu = !showMenu;
+	};
 </script>
 
-<header class="header u-unselectable header-animated">
-	<div class="header-brand">
-		<div class="nav-item no-hover">
-			<a href="/"><h1 class="title text-lg">PVC Prayer</h1></a>
-		</div>
-		<div class="nav-item nav-btn" id="header-btn">
-			<span></span>
-			<span></span>
-			<span></span>
-		</div>
-	</div>
-	<nav class="header-nav" id="header-menu">
-		<div class="nav-left">
-			<div class={`nav-item${$page.url.pathname === '/new' ? ' active' : ''}`}>
-				<a href="/new">New Prayer</a>
+<header class="mb-2 p-3">
+	<nav>
+		<div class="flex flex-nowrap items-center justify-between">
+			<div>
+				<a href="/">
+					<strong>PVC</strong> Prayer
+				</a>
 			</div>
-		</div>
-		<div class="nav-right">
-			{#if data.isLoggedIn}
-				<div class={`nav-item${$page.url.pathname === '/dashboard' ? ' active' : ''}`}>
-					<a href="/dashboard">Dashboard</a>
+			<div class="flex justify-end max-sm:hidden">
+				<ul>
+					<li>
+						<a href="/login">Admin</a>
+					</li>
+				</ul>
+			</div>
+			<div class="relative inline-block min-sm:hidden">
+				<button onclick={toggleShowMenu}>
+					{#if showMenu}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="size-6"
+						>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+						</svg>
+					{:else}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="size-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+							/>
+						</svg>
+					{/if}
+					<span class="sr-only">Open Menu</span>
+				</button>
+				<div
+					class={`${showMenu ? 'block' : 'hidden'} absolute -right-0 z-1 min-w-40 rounded-md bg-white shadow-md`}
+				>
+					<ul>
+						<li class="p-1 px-2">
+							<a href="/login">Admin</a>
+						</li>
+					</ul>
 				</div>
-				<div class={`nav-item${$page.url.pathname === '/prayer-list' ? ' active' : ''}`}>
-					<a href="/prayer-list">Prayer List</a>
-				</div>
-				<div class={`nav-item${$page.url.pathname === '/logout' ? ' active' : ''}`}>
-					<a href="/logout">Log out</a>
-				</div>
-			{:else}
-				<div class={`nav-item${$page.url.pathname === '/login' ? ' active' : ''}`}>
-					<a href="/login">Login as Admin</a>
-				</div>
-			{/if}
+			</div>
 		</div>
 	</nav>
 </header>
 
-<main>
-	<section>
-		<div class="content">
-			<slot />
-		</div>
-	</section>
+<main class="p-3 pt-0">
+	{@render children()}
 </main>
-<Toaster />
+
+<SvelteToast
+	options={{
+		duration: 20000
+	}}
+/>
